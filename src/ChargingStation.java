b822fd867a85fd331837f1161614b4c9449a515b
Olumide Oluwole <oluwoleit@gmail.com>
2023-12-01 01:52:20 +0100

    queue

--- src/ChargingStation.java
+++ src/ChargingStation.java
@@ -2,22 +2,28 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.concurrent.TimeUnit;
 
 // available resources = charging stations and their capacity
 public class ChargingStation implements Serializable {
 	private static final Logger LOGGER = Logger.getLogger(ChargingStation.class.getName());
+	 private static final int MAX_WAIT_TIME = 15; // Maximum wait time in minutes
 
     private int location;
     private boolean available;
     private int maxCapacity; // Maximum charging capacity of the station
     private int currentCapacity; // Current available charging slots
+    private List<Car> queue;
 
     public ChargingStation(int location, int maxCapacity) {
         this.location = location;
         this.maxCapacity = maxCapacity;
         this.available = true;
         this.currentCapacity = maxCapacity;
+        this.queue = new ArrayList<>();
     }
 
     public synchronized void occupy(Car car) {
@@ -40,8 +46,35 @@ public class ChargingStation implements Serializable {
         return currentCapacity > 0; // Check if there are available slots for charging
     }
     
-    public synchronized boolean getLocation() {
-        return location > 0; // Check if there are available slots for charging
-    } 
+//    public synchronized boolean getLocation() {
+//        return location > 0; // Check if there are available slots for charging
+//    } 
+    
+    public int getLocation() {
+        return location;
+    }
+
+    public synchronized int addToQueue(Car car) {
+    	   int waitingTime = queue.size() * 5; // Calculate waiting time (5 seconds per car in queue)
+           queue.add(car);
+           return waitingTime;
+    }
+    
+    public synchronized void simulateArrival(Car car) {
+        int waitingTime = addToQueue(car); // Add car to the station's queue
+
+        try {
+            if (waitingTime > MAX_WAIT_TIME * 60) {
+                LOGGER.info(car.getName() + " waited more than " + MAX_WAIT_TIME + " minutes and left the queue.");
+            } else {
+                LOGGER.info(car.getName() + " is waiting at station " + getLocation() + ". Waiting time: " + waitingTime / 60 + " minutes");
+                TimeUnit.SECONDS.sleep(5); // Simulating charging time
+                queue.remove(car); // Car finished charging, remove from queue
+                LOGGER.info(car.getName() + " finished charging and left the station.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
